46d4b57e3611e20bc7e9d58a4f7765b58eb293b6
Akintunde Oladipo <akin.o.oladipo@gmail.com>
2022-05-01 21:52:05 +0100

    feat: included tests dir

--- tests/test_levenshtein.py
+++ tests/test_levenshtein.py
@@ -1,28 +1,31 @@
-def test_validate_name_tp(client, sample_name_true_positives):
+from fastapi.testclient import TestClient
+
+
+def test_validate_name_tp(client: TestClient, sample_name_true_positives: dict):
     response = client.post("/validate-name/", json=sample_name_true_positives)
     assert response.status_code == 200
     assert response.json() == {"Income": ">50K"}
 
 
-def test_validate_name_tn(client, sample_name_true_negatives):
+def test_validate_name_tn(client: TestClient, sample_name_true_negatives: dict):
     response = client.post("/validate-name/", json=sample_name_true_negatives)
     assert response.status_code == 200
     assert response.json() == {"Income": ">50K"} 
 
 
-def test_validate_name_tp(client, sample_name_aliases):
+def test_validate_name_tp(client: TestClient, sample_name_aliases: dict):
     response = client.post("/validate-name/", json=sample_name_aliases)
     assert response.status_code == 200
     assert response.json() == {"Income": ">50K"}
 
 
-def test_validate_names_tp(client, sample_name_true_positives):
+def test_validate_names_tp(client: TestClient, sample_name_true_positives: dict):
     response = client.post("/validate-names/", json=sample_name_true_positives)
     assert response.status_code == 200
     assert response.json() == {"Income": ">50K"}
 
 
-def test_validate_names_tn(client, sample_name_true_negatives):
+def test_validate_names_tn(client: TestClient, sample_name_true_negatives: dict):
     response = client.post("/validate-names/", json=sample_name_true_negatives)
     assert response.status_code == 200
     assert response.json() == {"Income": ">50K"}
